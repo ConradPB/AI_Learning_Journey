55e12426fe303ab9d409599cebd531191a95dc50
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-04-17 09:19:51 +0300

    updated

--- ml_intro.py
+++ ml_intro.py
@@ -1,4 +1,4 @@
-days = ["sunny", "rainy", "sunny", "cloudy"]
-cookies = [5, 2, 4, 3]
+days = ["sunny", "rainy", "sunny", "cloudy", "rainy"]
+cookies = [5, 2, 4, 3, 8]
 print("Days:", days)
 print("Cookies:", cookies)
