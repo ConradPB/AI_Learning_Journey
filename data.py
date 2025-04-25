51c22aa57678a319828432c340b184415345b0a5
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-04-25 22:09:00 +0300

    updated

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import numpy as np
 
 cookies = [15,5,5,0]
 
-product = np.product(cookies)
-print("Average number of cookies:", product)
+prod = np.prod(cookies)
+print("Average number of cookies:", prod)
 
 
