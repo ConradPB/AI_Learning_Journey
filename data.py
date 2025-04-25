5cdb6d078a68edfb24b451f43c7d1ffe6d707419
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-04-26 00:42:02 +0300

    updated

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import numpy as np
 
 cookies = [15,5,5,70]
 
-average = np.average(cookies)
-print("Average number of cookies:", average)
+min = np.min(cookies)
+print("min number of cookies:", min)
 
 
