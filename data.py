7d7b241e655684c64937bc3b037fb35a84b4ca66
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-04-28 23:32:42 +0300

    updated

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-cookies = [15,5,5,70]
+cookies = [15,5,5,70, 54]
 
-jars = [12, 4, 13, 8]
+jars = [12, 4, 13, 8, 43]
 
 add = np.add(cookies, jars)
 print("total number of cookies:", add)
