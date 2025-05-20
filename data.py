12507ce7e1b1d79dcfaf855077009171b15001e2
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-05-20 20:21:12 +0300

    updated

--- data.py
+++ data.py
@@ -5,6 +5,6 @@ cookies = [15,5,15,70, 54, 0]
 jars = [12, 4, 13, 8, 43, 11]
 
 multiply = np.multiply(cookies, jars)
-print("total number of cookies:", add)
+print("total number of cookies:", multiply)
 
 
