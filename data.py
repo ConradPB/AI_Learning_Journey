224d0a8fd3a0d042319ccb33c4b468cfb956da12
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-05-22 22:30:50 +0300

    updated

--- data.py
+++ data.py
@@ -5,6 +5,6 @@ cookies = [15,5,15,70, 54, 0]
 jars = [12, 4, 13, 8, 43, 11]
 
 square = np.square(cookies, jars)
-print("total number of cookies:", subtract)
+print("total number of cookies:", square)
 
 
