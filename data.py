345f7f90861008c1351b03d8d13033a7250de820
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-05-24 23:00:14 +0300

    updated

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import numpy as np
 
 cookies = [15,5,15,70, 54, 0, 12]
 
-jars = [12, 4, 13, 8, 43, 11]
+jars = [12, 4, 13, 8, 43, 11, 9]
 
 square = np.square(jars)
 print("total number of jars:", square)
