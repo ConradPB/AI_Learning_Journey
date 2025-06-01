92cc1c13a678fbbbd633d3d4ffa07f4a2fdc32fd
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-06-01 22:45:31 +0300

    updated

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import numpy as np
 
 cookies = [151,54,15,70, 54, 0, 12]
 
-jars = [12, 4, 13, 8, 43, 11, 9]
+jars = [12, 4, 13, 8, 43, 11, 90]
 
 divide = np.divide(cookies, jars)
 print("total number of jars:", divide)
